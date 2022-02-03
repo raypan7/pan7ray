6591743d79b419574fb11c3bd69b6660d49feb27
RayPan <ray727035@gmail.com>
2022-02-03 20:56:05 +0800

    update vue loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,7 @@
-const { environment } = require("@rails/webpacker");
+const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
-const webpack = require("webpack");
-environment.plugins.append(
-  "Provide",
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery",
-    Popper: ["popper.js", "default"],
-  })
-);
+
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
-module.exports = environment;
+module.exports = environment
