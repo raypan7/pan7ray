f5ed9e4c9a7732446499c093e183ef523c6240f4
RayPan <ray727035@gmail.com>
2022-02-27 17:22:43 +0800

    Single Source of Truth & Removing local state

--- app/javascript/packs/src/counter.jsx
+++ app/javascript/packs/src/counter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Counter extends React.Component {
 	state = {
-		value: this.props.value,
+		value: this.props.counter.value,
 	};
 
 	handleIncrement = () => { // binding event handler
@@ -10,16 +10,17 @@ export default class Counter extends React.Component {
 	}
 
 	render() {
-		console.log("props:", this.props)
 		return (
 			<div>	
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button
-				  onClick={ () => this.handleIncrement()}
+				  onClick={ () => this.props.onIncrement(this.props.counter)}
 				  className="btn btn-s econdary btn-sm"
-				>
-					Increment
-				</button>
+				>Increment</button>
+				<button
+				  onClick={() => this.props.onDelete(this.props.counter.id)}
+					className='btn btn-danger btn-sm m-2'
+				>Delete</button>
 			</div>
 
 		);
@@ -32,7 +33,7 @@ export default class Counter extends React.Component {
 	}
 
 	formatCount() {
-		const { value } = this.state;
+		const { value } = this.props.counter;
 		return value === 0 ? 'zero' : value;
 	}
 }
