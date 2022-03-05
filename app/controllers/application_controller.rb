6d7a82b7659bdf9bdfb25b4e0b2c02a53dcb12d2
RayPan <ray727035@gmail.com>
2022-03-05 23:56:59 +0800

    fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_admin!
-    unless current_user.is_admin?
+    unless current_user.admin?
       flash[:alert] = '您的權限不足'
       redirect_to(root_path)
     end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,6 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   def can_deleted_by(user)
-    user && (user.is_admin? || user == self.author)
+    user && (user.admin? || user == author)
   end
 end
