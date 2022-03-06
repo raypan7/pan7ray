5dcc3ef18eff09978c785263d15dc2552b12912d
RayPan <ray727035@gmail.com>
2022-03-07 00:16:01 +0800

    Create POST功能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show update edit destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
     @comments = @post.comments
 
     if params[:keyword]
@@ -16,5 +17,32 @@ class PostsController < ApplicationController
     if params[:sort]
       @comments = @comments.order(params[:sort])
     end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    ActiveRecord::Base.transaction do
+      p_params = post_params.dup
+      @post = Post.new(p_params)
+      @post.save!
+      flash[:notice] = 'Created Successfully!'
+      redirect_to(posts_path)
+    rescue StandardError => e
+      flash[:alert] = "Create Failed，#{e}"
+      redirect_to(posts_path)
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
