1c0a2c25dfb0dfdabab07d9123ca5f3ad094ad80
RayPan <ray727035@gmail.com>
2022-03-06 18:58:10 +0800

    pic sum 隨機圖片

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,7 @@ require 'digest/md5'
 module UsersHelper
   def user_avatar_link(user)
     email_md5 = Digest::MD5.hexdigest(user.email)
-    gravatar_url = "https://www.gravatar.com/avatar/#{email_md5}"
+    gravatar_url = "https://picsum.photos/id/#{user.id + 120}/50"
 
     str = "<div class ='user-link'>" + link_to(image_tag(gravatar_url), user_path(user)) + " " + user.display_name + "</div>"
 
