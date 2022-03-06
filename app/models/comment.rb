22ca3d99685c231f43743e420f8eb241e6024a10
RayPan <ray727035@gmail.com>
2022-03-07 00:15:26 +0800

    default DESC

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
+  default_scope -> { order('comments.updated_at DESC') }
+
   def can_deleted_by(user)
     user && (user.admin? || user == author)
   end
